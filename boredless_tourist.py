49a27106ead9a1d56d13d06353eae28bf6db2456
Brian Mann <mannb986@gmail.com>
2021-03-21 16:12:45 +0100

    Created attractions and functionality for adding new attractions

--- boredless_tourist.py
+++ boredless_tourist.py
@@ -6,8 +6,8 @@ def get_destination_index(destination):
     destination_index = destinations.index(destination)
     return destination_index
 
-print(get_destination_index("Los Angeles, USA"))
-print(get_destination_index("Paris, France"))
+#print(get_destination_index("Los Angeles, USA"))
+#print(get_destination_index("Paris, France"))
 #print(get_destination_index("LHyderabad, India"))
 
 def get_traveler_location(traveler):
@@ -16,4 +16,30 @@ def get_traveler_location(traveler):
     return traveler_destination_index
 
 test_destination_index = get_traveler_location(test_traveler)
-print(test_destination_index)
+#print(test_destination_index)
+
+attractions = [[] for i in destinations]
+#print(attractions)
+
+def add_attraction(destination, attraction):
+    try:
+        destination_index = get_destination_index(destination)
+        attractions[destination_index].append(attraction)
+    except ValueError:
+        return
+
+add_attraction('Los Angeles, USA',['Venice Beach', ['beach']])
+
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+print(attractions)
+
